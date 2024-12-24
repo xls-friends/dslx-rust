2f74f3d2eae586905b87b34b8043f3a21e5ddd3f
Rob Springer <blenderkitty@gmail.com>
2024-12-23 18:14:02 -0800

    Restored accidentally-removed comment.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //! Full language defined here: <https://google.github.io/xls/dslx_reference/>
 //! Currently a spooky scary skeleton, but actively being built out.
 //!
+//! At present, the _only_ entry point is `parse_function_signature`, taking in an ast::ParseInput.
 // TODO one day when all functions in this file are used, delete below. For now, we prefer to
 // avoid spammy Github Actions notes about unused functions.
 #![allow(dead_code)]
